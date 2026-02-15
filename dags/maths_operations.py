ab91e87e9b6f39d3b45f7cd64f144cafa948fa7e
Prajwal Rudresh <prajwal.rudresh@gmail.com>
2026-02-14 20:17:00 -0600

    updated code

--- dags/maths_operations.py
+++ dags/maths_operations.py
@@ -10,12 +10,12 @@ Task 5: Compute the square of the result
 """
 
 from airflow import DAG
-from airflow.operators.python import PythonOperators
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 ## Define function for each task
 
-def start_number(**context):
+def start_number(ti, **context):
     context["ti"].xcom_push(key='current_value', value = 10)
 
 
@@ -23,27 +23,69 @@ def start_number(**context):
     print("The starting number is 10")
 
 
-def add_five(**context):
+def add_five(ti, **context):
     current_value = context["ti"].xcom_pull(key = 'current_value', task_ids ='start_task')
     new_value = current_value + 5
     context["ti"].xcom_push(key = 'current_value', value = new_value)
     print(f"Add 5: {current_value} +5 = {new_value}")
 
-def multiply_by_two(**context):
+def multiply_by_two(ti, **context):
     current_value = context["ti"].xcom_pull(key='current_value', task_ids = 'add_five_task')
     new_value = current_value*2
     context["ti"].xcom_push(key='current_value', value= new_value)
     print(f"Multiply by 2: {current_value} *2 = {new_value}")
 
-def subtract_by_three(**context):
+def subtract_by_three(ti, **context):
     current_value = context["ti"].xcom_pull(key='current_value', task_ids = 'multiply_by_two_task')
     new_value = current_value - 3
     context["ti"].xcom_push(key='current_value', value = new_value)
     print(f"Subtract 3: {current_value} - 3 = {new_value}")
     
 
-def square_number(**context):
+def square_number(ti, **context):
     current_value = context["ti"].xcom_pull(key='current_value', task_ids = 'subtract_by_three_task')
     new_value = current_value **2
-    
+    context["ti"].xcom_push(key = 'current_value', value = new_value)
     print(f"Square the result: {current_value}^2 = {new_value}")
+
+with DAG(
+    dag_id = 'math_sequence_dag',
+    start_date = datetime(2026,1,1),
+    schedule = '@once',
+    catchup = False,
+
+)as dag:
+    
+    # Define the task
+    start_task = PythonOperator(
+        task_id = 'start_task',
+        python_callable = start_number,
+        # provide_context = True 
+    )
+
+    add_five_task = PythonOperator(
+        task_id = 'add_five_task',
+        python_callable = add_five,
+        # provide_context = True
+    )
+
+    multiply_by_two_task = PythonOperator(
+        task_id = 'multiply_by_two_task',
+        python_callable = multiply_by_two,
+        # provide_context = True
+    )
+
+    subtract_by_three_task = PythonOperator(
+        task_id = 'subtract_by_three_task',
+        python_callable = subtract_by_three,
+        # provide_context = True
+    )
+
+    sqaure_number_task = PythonOperator(
+        task_id = 'sqaure_number_task',
+        python_callable = square_number,
+        # provide_context = True
+    )
+
+    ## Dependencies
+    start_task >> add_five_task >> multiply_by_two_task >> subtract_by_three_task >> sqaure_number_task
